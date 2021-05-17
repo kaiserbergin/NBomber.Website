9f547e9241eca9e0b3cdfbead62a7e603daa098f
Antya Dev <antyadev@gmail.com>
2021-05-17 09:57:23 +0300

    opened Blog section

--- src/docs/docusaurus.config.js
+++ src/docs/docusaurus.config.js
@@ -30,7 +30,7 @@ module.exports = {
           label: 'Docs',
           position: 'left',
         },           
-        //{to: 'blog', label: 'Blog', position: 'left'},
+        {to: 'blog', label: 'Blog', position: 'left'},
         {
           href: 'https://github.com/PragmaticFlow/NBomber',
           label: 'GitHub',
